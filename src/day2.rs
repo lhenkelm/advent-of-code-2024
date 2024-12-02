b3d5a3944c1101cc39853480330d7ca71b4ef5be
lhenkelm <lars.henkelmann@cern.ch>
2024-12-02 07:18:17 +0100

    test: day2 part2 example tests

--- src/day2.rs
+++ src/day2.rs
@@ -24,7 +24,28 @@ fn part_1_is_report_safe(report: &[u64]) -> bool {
         if prev.abs_diff(next) > 3 {
             return false;
         };
-        let this_sign = (next as i64 - prev as i64);
+        let this_sign = next as i64 - prev as i64;
+        if this_sign * sign < 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn part_2_is_report_safe(report: &[u64]) -> bool {
+    debug_assert!(report.len() > 1, "got report with less than two readings");
+    let n_pairs = report.len() - 1;
+    let sign = report[1] as i64 - report[0] as i64;
+    for i in 0..n_pairs {
+        let prev = report[i];
+        let next = report[i + 1];
+        if prev == next {
+            return false;
+        }
+        if prev.abs_diff(next) > 3 {
+            return false;
+        };
+        let this_sign = next as i64 - prev as i64;
         if this_sign * sign < 0 {
             return false;
         }
@@ -41,8 +62,11 @@ fn part1(input: &[Vec<u64>]) -> u64 {
 }
 
 #[aoc(day2, part2)]
-fn part2(input: &[Vec<u64>]) -> String {
-    todo!()
+fn part2(input: &[Vec<u64>]) -> u64 {
+    input
+        .iter()
+        .map(|report| part_2_is_report_safe(report) as u64)
+        .sum()
 }
 
 #[cfg(test)]
@@ -124,7 +148,51 @@ mod tests {
     }
 
     #[test]
+    fn part2_example_report1() {
+        let report = [7u64, 6, 4, 2, 1];
+        assert!(part_2_is_report_safe(&report));
+    }
+
+    #[test]
+    fn part2_example_report2() {
+        let report = [1u64, 2, 7, 8, 9];
+        assert!(part_2_is_report_safe(&report));
+    }
+
+    #[test]
+    fn part2_example_report3() {
+        let report = [9u64, 7, 6, 2, 1];
+        assert!(part_2_is_report_safe(&report));
+    }
+
+    #[test]
+    fn part2_example_report4() {
+        let report = [1u64, 3, 2, 4, 5];
+        assert!(!part_2_is_report_safe(&report));
+    }
+
+    #[test]
+    fn part2_example_report5() {
+        let report = [8u64, 6, 4, 4, 1];
+        assert!(!part_2_is_report_safe(&report));
+    }
+
+    #[test]
+    fn part2_example_report6() {
+        let report = [1u64, 3, 6, 7, 9];
+        assert!(part_2_is_report_safe(&report));
+    }
+
+    #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        let input = [
+            vec![7u64, 6, 4, 2, 1],
+            vec![1u64, 2, 7, 8, 9],
+            vec![9u64, 7, 6, 2, 1],
+            vec![1u64, 3, 2, 4, 5],
+            vec![8u64, 6, 4, 4, 1],
+            vec![1u64, 3, 6, 7, 9],
+        ];
+        assert_eq!(part2(&input), 4);
     }
 }
