5ff33ec8762046c9c761ca5a8642f8433f21ea39
lhenkelm <lars.henkelmann@cern.ch>
2024-12-04 20:39:32 +0100

    fix: did not notice example was square

--- src/day4.rs
+++ src/day4.rs
@@ -7,6 +7,10 @@ fn parse(input: &str) -> String {
 const XMAS: &'static str = "XMAS";
 const SAMX: &'static str = "SAMX";
 
+fn count_non_whitespace(s: &str) -> usize {
+    s.chars().filter(|c| !c.is_whitespace()).count()
+}
+
 #[aoc(day4, part1)]
 fn part1(input: &str) -> u64 {
     let mut occurances = input.matches(XMAS).count() as u64;
@@ -21,12 +25,14 @@ fn part1(input: &str) -> u64 {
         for &line_ in lines.iter() {
             transposed.push_str(&line_[col..col + 1]);
         }
-
         if col < n_cols - 1 {
             transposed.push('\n');
         }
     }
-    assert_eq!(transposed.len(), input.len());
+    assert_eq!(
+        count_non_whitespace(&transposed),
+        count_non_whitespace(&input)
+    );
     let transposed = transposed;
     occurances += transposed.matches(XMAS).count() as u64;
     occurances += transposed.matches(SAMX).count() as u64;
@@ -38,23 +44,27 @@ fn part1(input: &str) -> u64 {
             if row_idx >= n_rows {
                 break;
             }
+            dbg!((offset, i, row_idx));
             diagonals.push_str(&lines[row_idx][i..i + 1]);
         }
         diagonals.push('\n');
     }
-    for offset in 0..n_rows {
-        for i in 1..n_cols {
+    for offset in 1..n_cols {
+        for i in 0..n_rows {
             let col_idx = i + offset;
-            if col_idx >= n_cols {
+            if col_idx >= n_cols || i > n_rows - 1 {
                 break;
             }
             diagonals.push_str(&lines[i][col_idx..col_idx + 1]);
         }
-        if offset < n_rows - 1 {
+        if offset < n_cols - 1 {
             diagonals.push('\n');
         }
     }
-    assert_eq!(diagonals.len(), input.len() + n_cols);
+    assert_eq!(
+        count_non_whitespace(&diagonals),
+        count_non_whitespace(&input)
+    );
     let diagonals = diagonals;
     occurances += diagonals.matches(XMAS).count() as u64;
     occurances += diagonals.matches(SAMX).count() as u64;
@@ -71,20 +81,23 @@ fn part1(input: &str) -> u64 {
         }
         orthogonals.push('\n');
     }
-    for offset in 0..n_rows {
-        for i in 1..n_cols {
+    for offset in 1..n_cols {
+        for i in 0..n_rows {
             let col_idx = i + offset;
-            if col_idx >= n_cols {
+            if col_idx >= n_cols || i > n_rows - 1 {
                 break;
             }
             let rev_col_idx = n_cols - 1 - col_idx;
             orthogonals.push_str(&lines[i][rev_col_idx..rev_col_idx + 1]);
         }
-        if offset < n_rows - 1 {
+        if offset < n_cols - 1 {
             orthogonals.push('\n');
         }
     }
-    assert_eq!(orthogonals.len(), input.len() + n_cols);
+    assert_eq!(
+        count_non_whitespace(&orthogonals),
+        count_non_whitespace(&input)
+    );
     let orthogonals = orthogonals;
     occurances += orthogonals.matches(XMAS).count() as u64;
     occurances += orthogonals.matches(SAMX).count() as u64;
@@ -94,7 +107,7 @@ fn part1(input: &str) -> u64 {
 
 #[aoc(day4, part2)]
 fn part2(input: &str) -> String {
-    todo!()
+    String::new()
 }
 
 #[cfg(test)]
@@ -116,9 +129,22 @@ mod tests {
         MXMXAXMASX
     "};
 
+    const PART_1_SMALL_EXAMPLE: &'static str = indoc! {"
+        ..X...
+        .SAMX.
+        .A..A.
+        XMAS.S
+        .X....
+    "};
+
     #[test]
     fn part1_example() {
         assert_eq!(part1(&parse(PART_1_EXAMPLE)), 18);
+    }
+
+    #[test]
+    fn part1_small_example() {
+        assert_eq!(part1(&parse(PART_1_SMALL_EXAMPLE)), 4);
     }
 
     #[test]
