3abce0aea1bc55a5e72942b753b71191a4e0b3c8
lhenkelm <lars.henkelmann@cern.ch>
2024-12-07 10:09:02 +0100

    test: part 1 example e2e

--- src/day7.rs
+++ src/day7.rs
@@ -5,7 +5,7 @@ fn parse(input: &str) -> String {
 }
 
 #[aoc(day7, part1)]
-fn part1(input: &str) -> String {
+fn part1(input: &str) -> u64 {
     todo!()
 }
 
@@ -16,11 +16,25 @@ fn part2(input: &str) -> String {
 
 #[cfg(test)]
 mod tests {
+    use indoc::indoc;
+
     use super::*;
+
+    const PART_1_EXAMPLE_INPUT: &str = indoc! {"
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
+    "};
 
     #[ignore]
     fn part1_example() {
-        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part1(&parse(PART_1_EXAMPLE_INPUT)), 3749u64);
     }
 
     #[ignore]
