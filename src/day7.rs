d22170d338525a6dd9d28253eadfa43e46c5fe44
lhenkelm <lars.henkelmann@cern.ch>
2024-12-07 09:49:48 +0100

    chore: day 7 boilerplate

---
+++ src/day7.rs
@@ -0,0 +1,30 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+#[aoc_generator(day7)]
+fn parse(input: &str) -> String {
+    todo!()
+}
+
+#[aoc(day7, part1)]
+fn part1(input: &str) -> String {
+    todo!()
+}
+
+#[aoc(day7, part2)]
+fn part2(input: &str) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[ignore]
+    fn part1_example() {
+        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
+    }
+
+    #[ignore]
+    fn part2_example() {
+        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+    }
+}
