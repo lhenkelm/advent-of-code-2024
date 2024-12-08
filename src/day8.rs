b8d0a548d6e31067557796850eaae237d4c477e7
lhenkelm <lars.henkelmann@gmail.com>
2024-12-08 09:29:22 +0100

    chore: day 8 boilerplate

---
+++ src/day8.rs
@@ -0,0 +1,30 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+#[aoc_generator(day8)]
+fn parse(input: &str) -> String {
+    todo!()
+}
+
+#[aoc(day8, part1)]
+fn part1(input: &str) -> String {
+    todo!()
+}
+
+#[aoc(day8, part2)]
+fn part2(input: &str) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+    }
+}
