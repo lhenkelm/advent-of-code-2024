d85e170270bf4c64a00d1d9e3c043bfef0ad238a
lhenkelm <lars.henkelmann@gmail.com>
2024-12-09 09:15:38 +0100

    feat: I thought this should work (but it does not, somehow)

--- src/day9.rs
+++ src/day9.rs
@@ -1,3 +1,5 @@
+use std::iter;
+
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -23,12 +25,73 @@ fn parse(input: &str) -> Vec<DenseDiskValue> {
 }
 
 #[aoc(day9, part1)]
-fn part1(input: &Vec<DenseDiskValue>) -> u64 {
-    todo!()
+fn part1(input: &[DenseDiskValue]) -> u64 {
+    // a) Expand the compressed disk map representation
+    // 5 is not optimised, just guessed (files/empties can have beetween 1 and 9 blocks)
+    let mut sparse_disk_map = Vec::with_capacity(input.len() * 5);
+    let mut current_file_id = 0u64;
+    for val in input {
+        match val {
+            DenseDiskValue::Full(n_blocks) => {
+                sparse_disk_map
+                    .extend(iter::repeat(Some(current_file_id)).take(*n_blocks as usize));
+                current_file_id += 1;
+            }
+            DenseDiskValue::Empty(n_blocks) => {
+                sparse_disk_map.extend(iter::repeat(None).take(*n_blocks as usize));
+            }
+        }
+    }
+    let sparse_disk_map = sparse_disk_map;
+
+    // b) Find the compact allocation of files to blocks
+
+    // this one walks the disk map from the start, to either copy existing file IDs,
+    // or to identify a gap where a block from a file on the back may be inserted
+    let mut fwd_iter = sparse_disk_map.iter().enumerate();
+    // this one walks the disk map from the end, to find blocks of files that can be moved
+    // earlier to compress the used space
+    let mut bwd_iter = fwd_iter
+        .clone()
+        .filter_map(|(idx_bwd, id_opt)| match *id_opt {
+            Some(id) => Some((idx_bwd, id)),
+            None => None,
+        });
+    // another vector to store the result of file-compacting.
+    // Maybe there is a way to avoid the extra allocation?
+    // 3 is another uninformed guess based on what the example looks like
+    let mut compressed_disk_map = Vec::with_capacity(input.len() * 3);
+    loop {
+        let idx_fwd = loop {
+            let (idx_fwd, next_fwd) = fwd_iter
+                .next()
+                .expect("should never advance this one that far");
+            match next_fwd {
+                None => break idx_fwd,
+                Some(file_id) => compressed_disk_map.push(*file_id),
+            }
+        };
+
+        let (idx_bwd, next_file_back) = bwd_iter
+            .next_back()
+            .expect("should never retreat this one that far");
+        if idx_fwd >= idx_bwd {
+            break;
+        }
+        compressed_disk_map.push(next_file_back);
+    }
+
+    // c) compute the checksum
+    compressed_disk_map
+        .iter()
+        .enumerate()
+        .fold(0u64, |partial, (next_idx, next_file_id)| {
+            partial + next_idx as u64 * next_file_id
+        })
 }
 
 #[aoc(day9, part2)]
-fn part2(input: &Vec<DenseDiskValue>) -> String {
+fn part2(input: &[DenseDiskValue]) -> String {
     todo!()
 }
 
@@ -42,7 +105,7 @@ mod tests {
         2333133121414131402
     "};
 
-    #[ignore]
+    #[test]
     fn part1_example() {
         assert_eq!(part1(&parse(PART_1_EXAMPLE)), 1928u64);
     }
