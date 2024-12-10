295e140a17d62a54e1c66403b757acd8c73c7d6a
lhenkelm <lars.henkelmann@gmail.com>
2024-12-10 08:29:07 +0100

    feat: I hope they will be useful 😬

--- src/day10.rs
+++ src/day10.rs
@@ -1,4 +1,7 @@
+use std::ops::{Add, Index, IndexMut};
+
 use aoc_runner_derive::{aoc, aoc_generator};
+
 #[aoc_generator(day10)]
 fn parse(input: &str) -> String {
     todo!()
@@ -12,6 +15,61 @@ fn part1(input: &str) -> u64 {
 #[aoc(day10, part2)]
 fn part2(input: &str) -> String {
     todo!()
+}
+
+#[derive(Debug)]
+struct Grid<Item> {
+    data: Vec<Item>,
+    width: usize,
+    height: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+impl Add<(isize, isize)> for Point {
+    type Output = Point;
+
+    fn add(self, (dx, dy): (isize, isize)) -> Self::Output {
+        Point {
+            x: (self.x as isize + dx) as usize,
+            y: (self.y as isize + dy) as usize,
+        }
+    }
+}
+
+impl<Item> Index<Point> for Grid<Item> {
+    type Output = Item;
+
+    fn index(&self, point: Point) -> &Self::Output {
+        &self.data[point.y * self.width + point.x]
+    }
+}
+
+impl<Item> IndexMut<Point> for Grid<Item> {
+    fn index_mut(&mut self, point: Point) -> &mut Self::Output {
+        &mut self.data[point.y * self.width + point.x]
+    }
+}
+
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+impl Direction {
+    fn step(&self) -> (isize, isize) {
+        match self {
+            Direction::North => (0, -1),
+            Direction::East => (1, 0),
+            Direction::South => (0, 1),
+            Direction::West => (-1, 0),
+        }
+    }
 }
 
 #[cfg(test)]
