e13c8797d8f48fc4096551f61a53d9cf1bf98ea1
lhenkelm <lars.henkelmann@gmail.com>
2024-12-11 08:55:14 +0100

    test: day 11 part 1

---
+++ src/day11.rs
@@ -0,0 +1,32 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+#[aoc_generator(day11)]
+fn parse(input: &str) -> String {
+    todo!()
+}
+
+#[aoc(day11, part1)]
+fn part1(input: &str) -> u128 {
+    todo!()
+}
+
+#[aoc(day11, part2)]
+fn part2(input: &str) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_PART_1: &str = "125 17";
+
+    #[ignore]
+    fn part1_example() {
+        assert_eq!(part1(&parse(EXAMPLE_PART_1)), 55312);
+    }
+
+    #[ignore]
+    fn part2_example() {
+        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+    }
+}
