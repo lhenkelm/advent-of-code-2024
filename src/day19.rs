c85c957a31d138701a9c3846a775f47a81010aa4
lhenkelm <lars.henkelmann@gmail.com>
2024-12-19 11:37:24 +0100

    feat: parse

--- src/day19.rs
+++ src/day19.rs
@@ -1,16 +1,21 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools; // next_tuple
 #[aoc_generator(day19)]
-fn parse(input: &str) -> String {
-    todo!()
+fn parse(input: &str) -> (Vec<String>, Vec<String>) {
+    let input = input.trim().replace("\r\n", "\n");
+    let (towel_patterns, towel_designs): (&str, &str) = input.split("\n\n").next_tuple().unwrap();
+    let towel_patterns: Vec<String> = towel_patterns.split(", ").map(|s| s.to_string()).collect();
+    let towel_designs: Vec<String> = towel_designs.lines().map(|s| s.to_string()).collect();
+    (towel_patterns, towel_designs)
 }
 
 #[aoc(day19, part1)]
-fn part1(input: &str) -> u64 {
+fn part1((towel_patterns, towel_designs): &(Vec<String>, Vec<String>)) -> u64 {
     todo!()
 }
 
 #[aoc(day19, part2)]
-fn part2(input: &str) -> String {
+fn part2((towel_patterns, towel_designs): &(Vec<String>, Vec<String>)) -> String {
     todo!()
 }
 
