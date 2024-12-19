a50f19ee9c5f63065eefc50a75b8af85bb512343
lhenkelm <lars.henkelmann@gmail.com>
2024-12-19 12:41:24 +0100

    test: part 2

--- src/day19.rs
+++ src/day19.rs
@@ -50,8 +50,88 @@ mod tests {
         assert_eq!(part1(&parse(EXAMPLE)), 6);
     }
 
-    #[ignore]
+    #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE)), "<RESULT>");
+        assert_eq!(part2(&parse(EXAMPLE)), 16);
+    }
+
+    #[test]
+    fn part2_example_design_0() {
+        let example = indoc! {"
+            r, wr, b, g, bwu, rb, gb, br
+
+            brwrr
+        "};
+        assert_eq!(part2(&parse(example)), 2);
+    }
+
+    #[test]
+    fn part2_example_design_1() {
+        let example = indoc! {"
+            r, wr, b, g, bwu, rb, gb, br
+
+            bggr
+        "};
+        assert_eq!(part2(&parse(example)), 1);
+    }
+
+    #[test]
+    fn part2_example_design_2() {
+        let example = indoc! {"
+            r, wr, b, g, bwu, rb, gb, br
+
+            gbbr
+        "};
+        assert_eq!(part2(&parse(example)), 4);
+    }
+
+    #[test]
+    fn part2_example_design_3() {
+        let example = indoc! {"
+            r, wr, b, g, bwu, rb, gb, br
+
+            rrbgbr
+        "};
+        assert_eq!(part2(&parse(example)), 6);
+    }
+
+    #[test]
+    fn part2_example_design_4() {
+        let example = indoc! {"
+            r, wr, b, g, bwu, rb, gb, br
+
+            ubwu
+        "};
+        assert_eq!(part2(&parse(example)), 0);
+    }
+
+    #[test]
+    fn part2_example_design_5() {
+        let example = indoc! {"
+            r, wr, b, g, bwu, rb, gb, br
+
+            bwurrg
+        "};
+        assert_eq!(part2(&parse(example)), 1);
+    }
+
+    #[test]
+    fn part2_example_design_6() {
+        let example = indoc! {"
+            r, wr, b, g, bwu, rb, gb, br
+
+            brgr
+        "};
+        assert_eq!(part2(&parse(example)), 2);
+    }
+
+    #[test]
+    fn part2_example_design_7() {
+        let example = indoc! {"
+            r, wr, b, g, bwu, rb, gb, br
+
+            bbrgwb
+        "};
+        assert_eq!(part2(&parse(example)), 0);
     }
 }
