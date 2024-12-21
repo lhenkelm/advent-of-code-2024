1ca74b86977f7e95bf7aafbe48ee204b5ebf285a
lhenkelm <lars.henkelmann@gmail.com>
2024-12-21 17:08:23 +0100

    feat: parse inputs

--- src/day21.rs
+++ src/day21.rs
@@ -1,16 +1,25 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 #[aoc_generator(day21)]
-fn parse(input: &str) -> String {
-    todo!()
+fn parse(input: &str) -> [([char; 4], u32); 5] {
+    let input = input.trim().replace("\r\n", "\n");
+    let mut parsed = [([' '; 4], 0); 5];
+    for (i, line) in input.lines().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            parsed[i].0[j] = c;
+        }
+        parsed[i].1 = line[..3].parse().unwrap();
+    }
+
+    parsed
 }
 
 #[aoc(day21, part1)]
-fn part1(input: &str) -> u64 {
+fn part1(codes: &[([char; 4], u32); 5]) -> u64 {
     todo!()
 }
 
 #[aoc(day21, part2)]
-fn part2(input: &str) -> String {
+fn part2(input: &[([char; 4], u32); 5]) -> String {
     todo!()
 }
 
@@ -34,6 +43,6 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part2(&parse(EXAMPLE)), "<RESULT>");
     }
 }
