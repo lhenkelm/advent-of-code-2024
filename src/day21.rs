57f34877fc54da868c67a75cf87f5edf74fbd68c
lhenkelm <lars.henkelmann@gmail.com>
2024-12-23 21:32:50 +0100

    feat: works but does not scale, but scales a bit further

--- src/day21.rs
+++ src/day21.rs
@@ -1,6 +1,9 @@
 use std::collections::VecDeque;
+use std::fmt::Display;
+use std::iter;
 
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 use rustc_hash::{FxHashMap, FxHashSet};
 
 #[aoc_generator(day21)]
@@ -38,8 +41,188 @@ fn part1(codes: &[([char; 4], u64); 5]) -> u64 {
 }
 
 #[aoc(day21, part2)]
-fn part2(input: &[([char; 4], u64); 5]) -> String {
-    todo!()
+fn part2(input: &[([char; 4], u64); 5]) -> u64 {
+    const N_DIR_PAD_ROBOTS: usize = 4;
+
+    // 1) Find shortest path (not its length) between all immediately connected pairs
+    // of a NumPad and its nearest DirPad, in terms of the button presses required from the direction
+    // pad four levels above. The result is stored to be used later, to guide a greedy algorithm.
+    let mut shortest_paths_numpad = FxHashMap::default();
+    //  2 because we measure by button presses four levels above, and the "highest level"
+    // is modelled as the lowest index in the PadState::dir vector.
+    let dir_index_above = 2;
+    for from in NumPad::all() {
+        // TODO: Optimisation: since we visit every state every time, just store the results for the different `to`s
+        // in a map, and then use that map to find the shortest path to the `to`s.
+        for to in NumPad::all() {
+            let mut todo = VecDeque::new();
+            todo.push_back((
+                PadState {
+                    num: from,
+                    dir: vec![DirPad::A; 3],
+                },
+                vec![],
+            ));
+            let mut visited = FxHashSet::default();
+            let mut candidates = Vec::new();
+            while let Some((state, path)) = todo.pop_front() {
+                if !visited.insert(state.clone()) {
+                    continue;
+                }
+                if state.num == to {
+                    candidates.push((state.clone(), path.clone()));
+                }
+                for direction in DirPad::all() {
+                    if let Some(next_state) = state.clone().press(direction) {
+                        let mut new_path = path.clone();
+                        new_path.push(direction);
+                        todo.push_back((next_state, new_path));
+                    }
+                }
+            }
+            let shortest_path = candidates
+                .iter()
+                .min_by_key(|(_, path)| path.len())
+                .unwrap()
+                .1
+                .clone();
+            let mut path_one_level_up = Vec::new();
+            let mut state = PadState {
+                num: from,
+                dir: vec![DirPad::A; 3],
+            };
+            for dir in shortest_path.iter() {
+                state = state.clone().press(*dir).unwrap();
+                if dir == &DirPad::A && state.dir[..dir_index_above].iter().all(|d| d == &DirPad::A)
+                {
+                    path_one_level_up.push(state.dir[dir_index_above]);
+                }
+            }
+            shortest_paths_numpad.insert((from, to), path_one_level_up);
+        }
+    }
+    let shortest_paths_numpad = shortest_paths_numpad;
+    println!("Example best numpad paths:");
+    for (ft, tt) in [
+        (NumPad::A(false), NumPad::A(false)),
+        (NumPad::A(false), NumPad::A(true)),
+        (NumPad::A(false), NumPad::_0(false)),
+        (NumPad::A(false), NumPad::_0(true)),
+        (NumPad::A(false), NumPad::_3(false)),
+        (NumPad::A(false), NumPad::_3(true)),
+        (NumPad::A(false), NumPad::_8(true)),
+        (NumPad::_1(true), NumPad::_0(false)),
+    ] {
+        println!(
+            "{}\t=>{}\t: {}",
+            ft,
+            tt,
+            shortest_paths_numpad[&(ft, tt)].iter().format("")
+        );
+    }
+
+    // 2) Find shortest path (not its length) between all immediately connected pairs
+    // of a DirPad and its nearest DirPad, in terms of the button presses required from the direction
+    // pad four levels above. The result is stored to be used later, to guide a greedy algorithm.
+    let mut shortest_paths_dirpad = FxHashMap::default();
+    // 2 is then the index of the direction pad "immediately above" the relevant direction pad
+    let dir_index_above = 2;
+    for from in DirPad::all()
+        .into_iter()
+        .map(|d| PressableDirPad::from_dirpad(d, false))
+    {
+        for to in DirPad::all()
+            .into_iter()
+            .map(|d| PressableDirPad::from_dirpad(d, true))
+        {
+            let mut todo = VecDeque::new();
+            todo.push_back((DirPadStack::with_lowest(from, 3), vec![]));
+            let mut visited = FxHashSet::default();
+            let mut candidates = Vec::new();
+            while let Some((state, path)) = todo.pop_front() {
+                if !visited.insert(state.clone()) {
+                    continue;
+                }
+                if state.lowest == to {
+                    candidates.push((state.clone(), path.clone()));
+                }
+                for direction in DirPad::all() {
+                    if let Some(next_state) = state.clone().press(direction) {
+                        let mut new_path = path.clone();
+                        new_path.push(direction);
+                        todo.push_back((next_state, new_path));
+                    }
+                }
+            }
+            let shortest_path = candidates
+                .iter()
+                .min_by_key(|(_, path)| path.len())
+                .unwrap()
+                .1
+                .clone();
+            let mut path_one_level_up = Vec::new();
+            let mut state = DirPadStack::with_lowest(from, 3);
+            for dir in shortest_path.iter() {
+                state = state.clone().press(*dir).unwrap();
+                if dir == &DirPad::A
+                    && state.others[..dir_index_above]
+                        .iter()
+                        .all(|d| d == &DirPad::A)
+                {
+                    path_one_level_up.push(state.others[dir_index_above]);
+                }
+            }
+            shortest_paths_dirpad.insert((from.into_dirpad(), to.into_dirpad()), path_one_level_up);
+        }
+    }
+    let shortest_paths_dirpad = shortest_paths_dirpad;
+    println!("Example best dirpad paths:");
+    for (ft, tt) in [
+        (DirPad::A, DirPad::A),
+        (DirPad::A, DirPad::Up),
+        (DirPad::A, DirPad::Right),
+        (DirPad::A, DirPad::Left),
+        (DirPad::Left, DirPad::Up),
+    ] {
+        println!(
+            " {} \t=> {} \t: {}",
+            ft,
+            tt,
+            shortest_paths_dirpad[&(ft, tt)].iter().format("")
+        );
+    }
+
+    println!("line: {}", line!());
+    // 3) Greedy algorithm to find the shortest paths producing the given codes.
+    let mut total_complexity = 0;
+    for (code_seq, code_val) in input {
+        let mut code_complexity = 0;
+        let mut pad_state = PadState::new(N_DIR_PAD_ROBOTS);
+        for &code in code_seq {
+            let target_state = PadState::from_char(code, N_DIR_PAD_ROBOTS);
+            let mut path = shortest_paths_numpad[&(pad_state.num, target_state.num)].clone();
+            for _ in 0..N_DIR_PAD_ROBOTS {
+                let new_path = iter::once(DirPad::A)
+                    .chain(path.into_iter())
+                    .tuple_windows()
+                    .flat_map(|(from_dir, to_dir)| {
+                        shortest_paths_dirpad[&(from_dir, to_dir)].clone()
+                    })
+                    .collect();
+                path = new_path;
+            }
+            if N_DIR_PAD_ROBOTS < 5 {
+                println!("path: {}", path.iter().format(""));
+                for direction in path.iter() {
+                    pad_state = pad_state.press(*direction).unwrap();
+                }
+                assert_eq!(pad_state, target_state);
+            }
+            code_complexity += path.len() as u64;
+        }
+        total_complexity += code_complexity * code_val;
+    }
+    total_complexity
 }
 
 fn len_shortest_path_between_states(
@@ -76,6 +259,7 @@ fn len_shortest_path_between_states(
     }
     panic!("No path found");
 }
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct PadState {
     num: NumPad,
@@ -111,10 +295,112 @@ impl PadState {
         }
         let last_dir = *state.dir.last().unwrap();
         state.num = state.num.move_(last_dir)?;
-        if last_dir != DirPad::A {
+        Some(state)
+    }
+}
+
+impl Display for PadState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "@[{}|{}]", self.num, self.dir.iter().format(""))
+    }
+}
+
+// TODO: clean this up by unifying with the dirpad part of PadState
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct DirPadStack {
+    lowest: PressableDirPad,
+    others: Vec<DirPad>,
+}
+
+impl DirPadStack {
+    fn with_lowest(lowest: PressableDirPad, n: usize) -> Self {
+        Self {
+            lowest,
+            others: vec![DirPad::A; n],
+        }
+    }
+
+    fn press(self, direction: DirPad) -> Option<Self> {
+        let mut state = self;
+        *state.others.first_mut().unwrap() = state.others.first().unwrap().move_(direction)?;
+        if direction != DirPad::A {
             return Some(state);
         }
+        for i in 1..state.others.len() {
+            state.others[i] = state.others[i].move_(state.others[i - 1])?;
+            if state.others[i - 1] != DirPad::A {
+                return Some(state);
+            }
+        }
+        let last_other = *state.others.last().unwrap();
+        state.lowest = state.lowest.move_(last_other)?;
         Some(state)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum PressableDirPad {
+    Up(bool),
+    Down(bool),
+    Left(bool),
+    Right(bool),
+    A(bool),
+}
+
+impl PressableDirPad {
+    fn from_dirpad(dirpad: DirPad, pressed: bool) -> Self {
+        match dirpad {
+            DirPad::Up => Self::Up(pressed),
+            DirPad::Down => Self::Down(pressed),
+            DirPad::Left => Self::Left(pressed),
+            DirPad::Right => Self::Right(pressed),
+            DirPad::A => Self::A(pressed),
+        }
+    }
+
+    fn into_dirpad(self) -> DirPad {
+        match self {
+            Self::Up(_) => DirPad::Up,
+            Self::Down(_) => DirPad::Down,
+            Self::Left(_) => DirPad::Left,
+            Self::Right(_) => DirPad::Right,
+            Self::A(_) => DirPad::A,
+        }
+    }
+
+    fn move_(self, direction: DirPad) -> Option<Self> {
+        match (self, direction) {
+            (Self::Up(_), DirPad::Up) => None,
+            (Self::Up(_), DirPad::Left) => None,
+            (Self::Up(_), DirPad::Right) => Some(Self::A(false)),
+            (Self::Up(_), DirPad::Down) => Some(Self::Down(false)),
+            (Self::Up(false), DirPad::A) => Some(Self::Up(true)),
+            (Self::Up(true), DirPad::A) => Some(Self::Up(false)),
+            (Self::Down(_), DirPad::Up) => Some(Self::Up(false)),
+            (Self::Down(_), DirPad::Left) => Some(Self::Left(false)),
+            (Self::Down(_), DirPad::Right) => Some(Self::Right(false)),
+            (Self::Down(_), DirPad::Down) => None,
+            (Self::Down(false), DirPad::A) => Some(Self::Down(true)),
+            (Self::Down(true), DirPad::A) => Some(Self::Down(false)),
+            (Self::Left(_), DirPad::Up) => None,
+            (Self::Left(_), DirPad::Left) => None,
+            (Self::Left(_), DirPad::Right) => Some(Self::Down(false)),
+            (Self::Left(_), DirPad::Down) => None,
+            (Self::Left(false), DirPad::A) => Some(Self::Left(true)),
+            (Self::Left(true), DirPad::A) => Some(Self::Left(false)),
+            (Self::Right(_), DirPad::Up) => Some(Self::A(false)),
+            (Self::Right(_), DirPad::Left) => Some(Self::Down(false)),
+            (Self::Right(_), DirPad::Right) => None,
+            (Self::Right(_), DirPad::Down) => None,
+            (Self::Right(false), DirPad::A) => Some(Self::Right(true)),
+            (Self::Right(true), DirPad::A) => Some(Self::Right(false)),
+            (Self::A(_), DirPad::Up) => None,
+            (Self::A(_), DirPad::Left) => Some(Self::Up(false)),
+            (Self::A(_), DirPad::Right) => None,
+            (Self::A(_), DirPad::Down) => Some(Self::Right(false)),
+            (Self::A(false), DirPad::A) => Some(Self::A(true)),
+            (Self::A(true), DirPad::A) => Some(Self::A(false)),
+        }
     }
 }
 
@@ -149,6 +435,60 @@ impl NumPad {
             'A' => Self::A(true),
             _ => panic!("Invalid char"),
         }
+    }
+
+    fn format(&self) -> &str {
+        match self {
+            Self::_0(false) => "(0)",
+            Self::_0(true) => " 0 ",
+            Self::_1(false) => "(1)",
+            Self::_1(true) => " 1 ",
+            Self::_2(false) => "(2)",
+            Self::_2(true) => " 2 ",
+            Self::_3(false) => "(3)",
+            Self::_3(true) => " 3 ",
+            Self::_4(false) => "(4)",
+            Self::_4(true) => " 4 ",
+            Self::_5(false) => "(5)",
+            Self::_5(true) => " 5 ",
+            Self::_6(false) => "(6)",
+            Self::_6(true) => " 6 ",
+            Self::_7(false) => "(7)",
+            Self::_7(true) => " 7 ",
+            Self::_8(false) => "(8)",
+            Self::_8(true) => " 8 ",
+            Self::_9(false) => "(9)",
+            Self::_9(true) => " 9 ",
+            Self::A(false) => "(A)",
+            Self::A(true) => " A ",
+        }
+    }
+
+    fn all() -> [Self; 22] {
+        [
+            Self::_0(false),
+            Self::_0(true),
+            Self::_1(false),
+            Self::_1(true),
+            Self::_2(false),
+            Self::_2(true),
+            Self::_3(false),
+            Self::_3(true),
+            Self::_4(false),
+            Self::_4(true),
+            Self::_5(false),
+            Self::_5(true),
+            Self::_6(false),
+            Self::_6(true),
+            Self::_7(false),
+            Self::_7(true),
+            Self::_8(false),
+            Self::_8(true),
+            Self::_9(false),
+            Self::_9(true),
+            Self::A(false),
+            Self::A(true),
+        ]
     }
 
     fn move_(self, direction: DirPad) -> Option<Self> {
@@ -223,6 +563,12 @@ impl NumPad {
     }
 }
 
+impl Display for NumPad {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.format())
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum DirPad {
     Up,
@@ -233,6 +579,22 @@ enum DirPad {
 }
 
 impl DirPad {
+    fn all() -> [Self; 5] {
+        // sorted by reverse distance from A, such that expensive ops
+        // are resolved first
+        [Self::Left, Self::Down, Self::Up, Self::Right, Self::A]
+    }
+
+    fn char(&self) -> char {
+        match self {
+            Self::Up => '^',
+            Self::Down => 'v',
+            Self::Left => '<',
+            Self::Right => '>',
+            Self::A => 'A',
+        }
+    }
+
     fn move_(self, direction: DirPad) -> Option<Self> {
         match (self, direction) {
             (Self::Up, DirPad::Up) => None,
@@ -264,6 +626,11 @@ impl DirPad {
     }
 }
 
+impl Display for DirPad {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.char())
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -280,10 +647,5 @@ mod tests {
     #[test]
     fn part1_example() {
         assert_eq!(part1(&parse(EXAMPLE)), 126384);
-    }
-
-    #[test]
-    fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE)), "<RESULT>");
     }
 }
